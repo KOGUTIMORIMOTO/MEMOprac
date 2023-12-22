c5f5cfe6d7a89b21298ae5305c1a8fac6f2d069f
KOGUTIMORIMOTO <ayataro0903@gmail.com>
2023-12-22 17:57:09 +0900

    スペース5、6、7を追加

--- hello/index.js
+++ hello/index.js
@@ -30,6 +30,30 @@ function saveData4() {
     localStorage.setItem('savedData4_4', inputData4_4);
 }
 
+function saveData5() {
+    var inputData5 = document.getElementById('dataInput5').value;
+    localStorage.setItem('savedData5', inputData5);
+
+    var inputData5_5 = document.getElementById('dataInput5_5').value;
+    localStorage.setItem('savedData5_5', inputData5_5);
+}
+
+function saveData6() {
+    var inputData6 = document.getElementById('dataInput6').value;
+    localStorage.setItem('savedData6', inputData6);
+
+    var inputData6_6 = document.getElementById('dataInput6_6').value;
+    localStorage.setItem('savedData6_6', inputData6_6);
+}
+
+function saveData7() {
+    var inputData7 = document.getElementById('dataInput7').value;
+    localStorage.setItem('savedData7', inputData7);
+
+    var inputData7_7 = document.getElementById('dataInput7_7').value;
+    localStorage.setItem('savedData7_7', inputData7_7);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var savedData1_1 = localStorage.getItem('savedData1_1');
     document.getElementById('dataInput1_1').value = savedData1_1 || 'タイトルなし'; 
@@ -48,4 +72,19 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var savedData4_4 = localStorage.getItem('savedData4_4');
     document.getElementById('dataInput4_4').value = savedData4_4 || 'タイトルなし'; 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData5_5 = localStorage.getItem('savedData5_5');
+    document.getElementById('dataInput5_5').value = savedData5_5 || 'タイトルなし'; 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData6_6 = localStorage.getItem('savedData6_6');
+    document.getElementById('dataInput6_6').value = savedData6_6 || 'タイトルなし'; 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData7_7 = localStorage.getItem('savedData7_7');
+    document.getElementById('dataInput7_7').value = savedData7_7 || 'タイトルなし'; 
 });
